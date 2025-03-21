aac15f3d1b7808a780bbf44a7537c7cd63de31d5
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-21 22:34:47 +0100

    fix: rename files and update import to resolve syntax error

---
+++ _test_0001_two_sum.py
@@ -0,0 +1,7 @@
+from _0001_two_sum import Solution
+
+# Test cases
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
+print(solution.twoSum([3, 2, 4], 6))       # Output: [1, 2]
+print(solution.twoSum([3, 3], 6))          # Output: [0, 1]
