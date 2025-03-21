22b327e2f9fdab83e921edd5c68ba8d1294ba6e4
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-21 23:08:24 +0100

    perf: optimize longest substring without repeating characters

--- _0003_longest_substring_without_repeating_chars.py
+++ _0003_longest_substring_without_repeating_chars.py
@@ -1,12 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         char_index_map = {}
+        char_set = set()
         left = 0
         max_length = 0
 
         for right in range(len(s)):
-            if s[right] in char_index_map and char_index_map[s[right]] >= left:
-                left = char_index_map[s[right]] + 1
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
             char_index_map[s[right]] = right
             max_length = max(max_length, right - left + 1)
 
