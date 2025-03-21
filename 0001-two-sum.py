ed504b848931963b05a44fbf7440dc965560be10
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-21 22:33:35 +0100

    refactor: move twoSum function into Solution class and extract test cases

--- 0001-two-sum.py
+++ 0001-two-sum.py
@@ -1,12 +1,13 @@
-def two_sum(nums, target):
-    num_to_index = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in num_to_index:
-            return [num_to_index[complement], i]
-        num_to_index[num] = i
-
-# Test cases
-print(two_sum([2, 7, 11, 15], 9))  # Output: [0, 1]
-print(two_sum([3, 2, 4], 6))       # Output: [1, 2]
-print(two_sum([3, 3], 6))          # Output: [0, 1]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
---
+++ test_0001-two-sum.py
@@ -0,0 +1,7 @@
+from 0001-two-sum import Solution
+
+# Test cases
+solution = Solution()
+print(solution.twoSum([2, 7, 11, 15], 9))  # Output: [0, 1]
+print(solution.twoSum([3, 2, 4], 6))       # Output: [1, 2]
+print(solution.twoSum([3, 3], 6))          # Output: [0, 1]
