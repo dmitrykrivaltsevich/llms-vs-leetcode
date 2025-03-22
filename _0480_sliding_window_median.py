a11069260d1febf952b2d4ce36e1995063cedc8b
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-22 10:22:44 +0100

    refactor: replace heaps with sorted lists for median calculation

--- _0480_sliding_window_median.py
+++ _0480_sliding_window_median.py
@@ -1,53 +1,49 @@
 from typing import List
-import heapq
 
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
         if not nums or k == 0:
             return []
 
-        # Min-heap for the larger half of the numbers
-        max_heap = []
-        # Max-heap for the smaller half of the numbers (inverted to use min-heap)
-        min_heap = []
+        # Two sorted lists to maintain the lower and upper halves of the window elements
+        lower = []
+        upper = []
 
-        def add_num(num):
-            if not max_heap or num <= -max_heap[0]:
-                heapq.heappush(max_heap, -num)
+        def insert(num):
+            if not lower or num <= lower[-1]:
+                lower.append(num)
             else:
-                heapq.heappush(min_heap, num)
+                upper.append(num)
 
-            # Balance the heaps
-            if len(max_heap) > len(min_heap) + 1:
-                heapq.heappush(min_heap, -heapq.heappop(max_heap))
-            elif len(min_heap) > len(max_heap):
-                heapq.heappush(max_heap, -heapq.heappop(min_heap))
+            # Balance the lists
+            if len(lower) > len(upper) + 1:
+                upper.append(lower.pop())
+            elif len(upper) > len(lower):
+                lower.append(upper.pop())
 
-        def remove_num(num):
-            if num <= -max_heap[0]:
-                max_heap.remove(-num)
-                heapq.heapify(max_heap)
+        def remove(num):
+            if num <= lower[-1]:
+                lower.remove(num)
             else:
-                min_heap.remove(num)
-                heapq.heapify(min_heap)
+                upper.remove(num)
 
-            # Balance the heaps
-            if len(max_heap) > len(min_heap) + 1:
-                heapq.heappush(min_heap, -heapq.heappop(max_heap))
-            elif len(min_heap) > len(max_heap):
-                heapq.heappush(max_heap, -heapq.heappop(min_heap))
+            # Balance the lists
+            if len(lower) > len(upper) + 1:
+                upper.append(lower.pop())
+            elif len(upper) > len(lower):
+                lower.append(upper.pop())
 
         def get_median():
             if k % 2 == 1:
-                return float(-max_heap[0])
+                return float(lower[-1])
             else:
-                return (-max_heap[0] + min_heap[0]) / 2
+                return (lower[-1] + upper[0]) / 2
 
         result = []
         for i in range(len(nums)):
-            add_num(nums[i])
+            insert(nums[i])
             if i >= k - 1:
                 result.append(get_median())
-                remove_num(nums[i - (k - 1)])
+                remove(nums[i - (k - 1)])
 
         return result
