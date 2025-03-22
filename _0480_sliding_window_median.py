c10dd2b02fcd7b2c7128998ac79d2c51700d50d8
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-22 15:12:52 +0100

    feat: implement sliding window median algorithm

--- _0480_sliding_window_median.py
+++ _0480_sliding_window_median.py
@@ -37,5 +37,48 @@
 #
 # Make edits below this line only
 #
+
+from typing import List
+import heapq
+
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        if not nums or k == 0:
+            return []
+
+        max_heap = []  # Max-heap for the smaller half of numbers
+        min_heap = []  # Min-heap for the larger half of numbers
+
+        def add_num(num):
+            if len(max_heap) == 0 or num <= -max_heap[0]:
+                heapq.heappush(max_heap, -num)
+            else:
+                heapq.heappush(min_heap, num)
+
+            # Balance the heaps
+            if len(max_heap) > len(min_heap) + 1:
+                heapq.heappush(min_heap, -heapq.heappop(max_heap))
+            elif len(min_heap) > len(max_heap):
+                heapq.heappush(max_heap, -heapq.heappop(min_heap))
+
+        def get_median():
+            if k % 2 == 1:
+                return -max_heap[0]
+            else:
+                return (-max_heap[0] + min_heap[0]) / 2
+
+        result = []
+        for i in range(len(nums)):
+            add_num(nums[i])
+            if i >= k - 1:
+                result.append(get_median())
+                # Remove the element that is sliding out of the window
+                num_to_remove = nums[i - (k - 1)]
+                if num_to_remove <= get_median():
+                    max_heap.remove(-num_to_remove)
+                    heapq.heapify(max_heap)
+                else:
+                    min_heap.remove(num_to_remove)
+                    heapq.heapify(min_heap)
+
+        return result
