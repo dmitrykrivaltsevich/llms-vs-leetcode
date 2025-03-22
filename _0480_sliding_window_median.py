90f3e234209d4ef4fe53cbc0b05921dcfbd2a8fc
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-22 17:58:30 +0100

    feat: add element counts to heaps for efficient removal

--- _0480_sliding_window_median.py
+++ _0480_sliding_window_median.py
@@ -48,26 +48,44 @@ class Solution:
 
         max_heap = []  # Max-heap for the smaller half of numbers
         min_heap = []  # Min-heap for the larger half of numbers
+        count_max = {}
+        count_min = {}
 
         def add_num(num):
             if len(max_heap) == 0 or num <= -max_heap[0]:
                 heapq.heappush(max_heap, -num)
+                count_max[num] = count_max.get(num, 0) + 1
             else:
                 heapq.heappush(min_heap, num)
+                count_min[num] = count_min.get(num, 0) + 1
 
             # Balance the heaps
             if len(max_heap) > len(min_heap) + 1:
                 val = -heapq.heappop(max_heap)
                 heapq.heappush(min_heap, val)
+                count_max[val] -= 1
+                if count_max[val] == 0:
+                    del count_max[val]
+                count_min[val] = count_min.get(val, 0) + 1
             elif len(min_heap) > len(max_heap):
                 val = heapq.heappop(min_heap)
                 heapq.heappush(max_heap, -val)
+                count_min[val] -= 1
+                if count_min[val] == 0:
+                    del count_min[val]
+                count_max[-val] = count_max.get(-val, 0) + 1
 
         def remove_num(num):
             if num <= get_median():
+                count_max[num] -= 1
+                if count_max[num] == 0:
+                    del count_max[num]
                 max_heap.remove(-num)
                 heapq.heapify(max_heap)
             else:
+                count_min[num] -= 1
+                if count_min[num] == 0:
+                    del count_min[num]
                 min_heap.remove(num)
                 heapq.heapify(min_heap)
 
