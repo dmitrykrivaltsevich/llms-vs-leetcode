84b0805eec925ab49d4009fa328df7423c900850
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-22 09:48:00 +0100

    refactor: optimize space usage in regular expression matching

--- _0010_regular_expression_matching.py
+++ _0010_regular_expression_matching.py
@@ -1,7 +1,7 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp = [[False] * (n + 1) for _ in range(2)]
 
         # Base case: empty pattern matches empty string
         dp[0][0] = True
@@ -15,11 +15,11 @@ class Solution:
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if p[j - 1] == s[i - 1] or p[j - 1] == '.':
-                    dp[i][j] = dp[i - 1][j - 1]
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1]
                 elif p[j - 1] == '*':
                     # Two cases:
                     # 1. '*' matches zero occurrence of the preceding element
                     # 2. '*' matches one or more occurrences of the preceding element
-                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and (p[j - 2] == s[i - 1] or p[j - 2] == '.'))
+                    dp[i % 2][j] = dp[i % 2][j - 2] or (dp[(i - 1) % 2][j] and (p[j - 2] == s[i - 1] or p[j - 2] == '.'))
 
-        return dp[m][n]
+        return dp[m % 2][n]
