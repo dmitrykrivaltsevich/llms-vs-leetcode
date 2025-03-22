81be10646b1e3582fc1e3d909193bc3c74766f5b
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-22 15:16:50 +0100

    feat: add efficient element removal for sliding window median

--- _0480_sliding_window_median.py
+++ _0480_sliding_window_median.py
@@ -48,18 +48,46 @@ class Solution:
 
         max_heap = []  # Max-heap for the smaller half of numbers
         min_heap = []  # Min-heap for the larger half of numbers
+        max_count = {}
+        min_count = {}
 
         def add_num(num):
             if len(max_heap) == 0 or num <= -max_heap[0]:
                 heapq.heappush(max_heap, -num)
+                if -num in max_count:
+                    max_count[-num] += 1
+                else:
+                    max_count[-num] = 1
             else:
                 heapq.heappush(min_heap, num)
+                if num in min_count:
+                    min_count[num] += 1
+                else:
+                    min_count[num] = 1
 
             # Balance the heaps
             if len(max_heap) > len(min_heap) + 1:
-                heapq.heappush(min_heap, -heapq.heappop(max_heap))
+                val = -heapq.heappop(max_heap)
+                heapq.heappush(min_heap, val)
+                max_count[val] -= 1
+                if max_count[val] == 0:
+                    del max_count[val]
             elif len(min_heap) > len(max_heap):
-                heapq.heappush(max_heap, -heapq.heappop(min_heap))
+                val = heapq.heappop(min_heap)
+                heapq.heappush(max_heap, -val)
+                min_count[val] -= 1
+                if min_count[val] == 0:
+                    del min_count[val]
+
+        def remove_num(num):
+            if num <= get_median():
+                max_count[-num] -= 1
+                if max_count[-num] == 0:
+                    del max_count[-num]
+            else:
+                min_count[num] -= 1
+                if min_count[num] == 0:
+                    del min_count[num]
 
         def get_median():
             if k % 2 == 1:
@@ -73,12 +101,6 @@ class Solution:
             if i >= k - 1:
                 result.append(get_median())
                 # Remove the element that is sliding out of the window
-                num_to_remove = nums[i - (k - 1)]
-                if num_to_remove <= get_median():
-                    max_heap.remove(-num_to_remove)
-                    heapq.heapify(max_heap)
-                else:
-                    min_heap.remove(num_to_remove)
-                    heapq.heapify(min_heap)
+                remove_num(nums[i - (k - 1)])
 
         return result
