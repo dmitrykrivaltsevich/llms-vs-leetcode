947f59cd9cf93cd1a2eb1e8f43aaa3fd647c6889
Dmitry Krivaltsevich <dmitry.krivaltsevich@gmail.com>
2025-03-23 02:01:36 +0100

    feat: implement smallestNumber function with zero-free digit product check

--- _3348_smallest_divisible_digit_product_2.py
+++ _3348_smallest_divisible_digit_product_2.py
@@ -28,5 +28,50 @@
 #  2 Use these prime factors to construct the smallest number greater than or equal to num that is divisible by t. 
 # Make edits below this line only. Good code contains docstrings, comments, and correct indentation.
 
+from math import gcd
+from functools import reduce
+
 class Solution:
     def smallestNumber(self, num: str, t: int) -> str:
+
+        def prime_factors(n):
+            """Return the prime factors of n."""
+            i = 2
+            factors = []
+            while i * i <= n:
+                if n % i:
+                    i += 1
+                else:
+                    n //= i
+                    factors.append(i)
+            if n > 1:
+                factors.append(n)
+            return factors
+
+        def product_of_digits(s):
+            """Return the product of digits in string s."""
+            return reduce(lambda x, y: x * int(y), s, 1)
+
+        def is_zero_free(s):
+            """Check if the string s contains any zeroes."""
+            return '0' not in s
+
+        def next_number(s):
+            """Generate the next number greater than the given number s."""
+            num = int(s)
+            while True:
+                num += 1
+                yield str(num)
+
+        # Generate prime factors of t
+        factors = prime_factors(t)
+
+        # Convert num to integer for easier manipulation
+        current_num = int(num)
+
+        # Iterate over possible numbers greater than or equal to num
+        for candidate in next_number(num):
+            if is_zero_free(candidate) and all(factor in prime_factors(product_of_digits(candidate)) for factor in factors):
+                return candidate
+
+        return "-1"
